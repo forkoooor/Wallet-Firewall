d15b1d0edbcb2112386bd34bce2953b1258a0817
fun <lljxx1@gmail.com>
2022-06-15 13:22:33 +0800

    update bundle

--- bundle/bundle.js
+++ bundle/bundle.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         Web3 Wallet Firewall
 // @namespace    https://scamsniffer.io
-// @version      0.1
+// @version      0.0.2
 // @description  Wallet firewall is a firewall built between wallet and dapp, it can help us to observe the requests between dapp and wallet, on top of that we can add some protection rules like: if we find dapp request setApprovalForAll, and the domain name of dapp is not in the domain whitelist, remind the user to confirm the risk, etc.
 // @author       fun
 // @match        *://*
